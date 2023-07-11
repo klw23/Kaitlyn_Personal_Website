a5134b2edfc83404a2b7af7edc10f0bace0de318
Kaitlyn Wong <kaitlyn23wong@gmail.com>
2023-07-11 16:57:55 -0400

    changes

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import Navigation from '../Navigation'
