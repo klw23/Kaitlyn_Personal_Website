ba812b14c092981ebf8f8b60e27ac286c6474006
Kaitlyn Wong <kaitlyn23wong@gmail.com>
2023-07-11 22:22:21 -0400

    changes

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -19,52 +19,56 @@ function About() {
               </div>
             </div>
             <div className="col-sm-12 col-md-12 col-lg-6">
-              <div className="row align-items-center justify-content-center text-center">
+              <div className="row align-items-center justify-content-start text-center"
+                  style={{
+                     paddingLeft: "25px",
+                  }}              
+                >
                 <div className="col-sm-12 col-md-12 col-lg-10">
-                  <h1 className="about-page-header-title"> {data[0]?.name}</h1>
-                  <p className="about-page-description">
-                    {data[0]?.description}
-                  </p>
-                  <a href={data[0]?.pathToResume} download={data[0]?.resume}>
-                    <button className="about-page-download-resume-button"> DOWNLOAD RESUME </button>
-                  </a>
-  
-                  <h1 className="about-page-header-title"> EDUCATION </h1>
-                  <p className="about-page-education-technology">
-                    <span className="about-page-list-title"> Relevant Courses </span>
-                    <br></br>
-                    {data[0]?.relevantCourses.map((course) => (
-                      <span> {course} </span>
-                    ))}
-                  </p>
-    
-                  <h1 className="about-page-header-title"> TECHNOLOGY </h1>
-                  <p className="about-page-education-technology">
-                  <span className="about-page-list-title"> Languages </span>
-                    <br></br>
-                    {data[0]?.languages.map((app) => (
-                      <span> {app} </span>
-                    ))}
-                    <br></br>
-                    <br></br>
-                    <span className="about-page-list-title"> Frameworks and Libraries  </span>
-                    <br></br>
-                    {data[0]?.frameworksAndLibraries.map((app) => (
-                      <span> {app} </span>
-                    ))}
-                    <br></br>
-                    <br></br>
-                    <span className="about-page-list-title"> Applications and Systems </span>
-                    <br></br>
-                    {data[0]?.appsAndSystems.map((app) => (
-                      <span> {app} </span>
-                    ))}
-                  </p>
-                </div>
-              </div>
+              <h1 className="about-page-header-title"> {data[0]?.name}</h1>
+              <p className="about-page-description">
+                {data[0]?.description}
+              </p>
+              <a href={data[0]?.pathToResume} download={data[0]?.resume}>
+                <button className="about-page-download-resume-button"> DOWNLOAD RESUME </button>
+              </a>
+
+              <h1 className="about-page-header-title"> EDUCATION </h1>
+              <p className="about-page-education-technology">
+                <span className="about-page-list-title"> Relevant Courses </span>
+                <br></br>
+                {data[0]?.relevantCourses.map((course) => (
+                  <span> {course} </span>
+                ))}
+              </p>
+
+              <h1 className="about-page-header-title"> TECHNOLOGY </h1>
+              <p className="about-page-education-technology">
+                <span className="about-page-list-title"> Languages </span>
+                <br></br>
+                {data[0]?.languages.map((app) => (
+                  <span> {app} </span>
+                ))}
+                <br></br>
+                <br></br>
+                <span className="about-page-list-title"> Frameworks and Libraries  </span>
+                <br></br>
+                {data[0]?.frameworksAndLibraries.map((app) => (
+                  <span> {app} </span>
+                ))}
+                <br></br>
+                <br></br>
+                <span className="about-page-list-title"> Applications and Systems </span>
+                <br></br>
+                {data[0]?.appsAndSystems.map((app) => (
+                  <span> {app} </span>
+                ))}
+              </p>
             </div>
           </div>
         </div>
+      </div>
+        </div >
       </>
     );
   }
